190c64a2ac381f83a73e71f14cccb9fc283bc50e
Kun Luo <olooook@outlook.com>
2021-09-15 10:36:07 +0800

    .

--- spiral-matrix.cc
+++ spiral-matrix.cc
@@ -4,40 +4,64 @@ using namespace std;
 
 class Solution {
   public:
-    vector<int> spiralOrder(vector<vector<int>> &matrix) {
+    vector<int> spiralOrder(const vector<vector<int>>& matrix) {
         vector<int> res;
-        if (matrix.empty()) return res;
+        if (matrix.empty()) {
+            return res;
+        }
         res.reserve(matrix.size() * matrix[0].size());
-        int  top = 0, left = 0, bottom = matrix.size() - 1, right = matrix[0].size() - 1;
-        auto cur = 0;
-        while (bottom >= top || right >= left) {
-            if (bottom >= top) {
-                for (cur = left; cur <= right; ++cur) res.push_back(matrix[top][cur]);
+        int64_t top = 0, lft = 0, bot = matrix.size() - 1, rgt = matrix[0].size() - 1;
+        int64_t cur = 0;
+        while (bot >= top || rgt >= lft) {
+            if (bot >= top) {
+                for (cur = lft; cur <= rgt; ++cur) {
+                    res.push_back(matrix[top][cur]);
+                }
             }
             ++top;
-            if (right >= left) {
-                for (cur = top; cur <= bottom; ++cur) res.push_back(matrix[cur][right]);
+            if (rgt >= lft) {
+                for (cur = top; cur <= bot; ++cur) {
+                    res.push_back(matrix[cur][rgt]);
+                }
             }
-            --right;
-            if (bottom >= top) {
-                for (cur = right; cur >= left; --cur) res.push_back(matrix[bottom][cur]);
+            --rgt;
+            if (bot >= top) {
+                for (cur = rgt; cur >= lft; --cur) {
+                    res.push_back(matrix[bot][cur]);
+                }
             }
-            --bottom;
-            if (right >= left) {
-                for (cur = bottom; cur >= top; --cur) res.push_back(matrix[cur][left]);
+            --bot;
+            if (rgt >= lft) {
+                for (cur = bot; cur >= top; --cur) {
+                    res.push_back(matrix[cur][lft]);
+                }
             }
-            ++left;
+            ++lft;
         }
         return res;
     }
 };
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
     Solution            solution;
-    vector<vector<int>> matrix0{{1, 1, 1, 1, 1}, {2, 2, 2, 2, 2}, {3, 3, 3, 3, 3}, {4, 4, 4, 4, 4}};
-    vector<vector<int>> matrix1{{1}, {2}, {3}, {4}};
+    vector<vector<int>> matrix0{
+        {1, 1, 1, 1, 1},
+        {2, 2, 2, 2, 2},
+        {3, 3, 3, 3, 3},
+        {4, 4, 4, 4, 4},
+    };
+    vector<vector<int>> matrix1{
+        {1},
+        {2},
+        {3},
+        {4},
+    };
 
-    for (auto &&value : solution.spiralOrder(matrix0)) { cout << value << " "; }
+    for (auto&& value : solution.spiralOrder(matrix0)) {
+        cout << value << " ";
+    }
     cout << endl;
-    for (auto &&value : solution.spiralOrder(matrix1)) { cout << value << " "; }
+    for (auto&& value : solution.spiralOrder(matrix1)) {
+        cout << value << " ";
+    }
 }
